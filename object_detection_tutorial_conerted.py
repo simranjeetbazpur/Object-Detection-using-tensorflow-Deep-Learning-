f4d7a9f3b96de65aa332137f4bd817c6c9abba5b
Simranjeet Singh Randhawa <37202838+simranjeetbazpur@users.noreply.github.com>
2019-03-27 12:27:15 -0600

    Update object_detection_tutorial_conerted.py

--- object_detection_tutorial_conerted.py
+++ object_detection_tutorial_conerted.py
@@ -1,4 +1,7 @@
 
+#Reference: 1. https://github.com/tensorflow/models/tree/master/research/object_detection
+#           2. https://github.com/EdjeElectronics/TensorFlow-Object-Detection-API-Tutorial-Train-Multiple-Objects-Windows-10
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -7,26 +10,20 @@ import tarfile
 import tensorflow as tf
 from utils import label_map_util
 from utils import visualization_utils as vis_util
-#import zipfile
 
-#from collections import defaultdict
-#from io import StringIO
-#from matplotlib import pyplot as plt
-#from PIL import Image   #is required when using the model for static images.
-
+from matplotlib import pyplot as plt
+from PIL import Image   #is required when using the model for static images.
 import cv2
-cap = cv2.VideoCapture(0)
 
-# This is needed since the notebook is stored in the object_detection folder.
+video = cv2.VideoCapture(0)
+
+# This is needed.
 sys.path.append("..")
 
 
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
 # # Model preparation 
 
-# We tested with  "SSD with Mobilenet" model and faster_rcnn_inception_v2_coco_2018_01_28 using webcam. 
+# We tested with  "SSD with Mobilenet & faster_rcnn_inception_v2_coco" model and faster_rcnn_inception_v2_coco_2018_01_28 using webcam. 
 
 model = 'faster_rcnn_inception_v2_coco_2018_01_28'
 model_tar = model + '.tar.gz'
@@ -39,18 +36,18 @@ path_to_model = model + '/frozen_inference_graph.pb'
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 #number of classes to be identified
-NUM_CLASSES = 45
+NUM_CLASSES = 90
 
 
 # Model gets downloaded
 
-opener = urllib.request.URLopener()
-opener.retrieve(download_url + model_tar, model_tar)
-tar_file = tarfile.open(model_tar)
-for file in tar_file.getmembers():
+opener_web = urllib.request.URLopener()
+opener_web.retrieve(download_url + model_tar, model_tar)
+tarfile = tarfile.open(model_tar)
+for file in tarfile.getmembers():
   file_name = os.path.basename(file.name)
   if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
+    tarfile.extract(file, os.getcwd())
 
 
 # ## Loading this Tensorflow model into the memory
@@ -63,19 +60,14 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-
 # ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
+# Label maps map indices to category name
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-
 # ## Helper code
-
-# In[8]:
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
@@ -83,52 +75,43 @@ def load_image_into_numpy_array(image):
       (im_height, im_width, 3)).astype(np.uint8)
 
 
-# # Detection
-
-# In[9]:
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+# Test on static images
+#PATH_TO_TEST_IMAGES_DIR = 'test_images'
+#TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
 
 # Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
-
-# In[10]:
+#IMAGE_SIZE = (12, 8)
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
-    while True:
-      ret, image_np = cap.read()
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    a=True;
+    while a:
+      ret, image_np = video.read()
+      # Expand dimensions since the model expects images shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      
       # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      detected_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
       # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      detected_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      tensorimage = detection_graph.get_tensor_by_name('image_tensor:0')
+      detected_classes = detection_graph.get_tensor_by_name('detection_classes:0')
       num_detections = detection_graph.get_tensor_by_name('num_detections:0')
       # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
+      ( detected_classes,detected_boxes, detected_scores, num_detections) = sess.run(
+          [ detected_classes,detected_boxes, detected_scores, num_detections],
+          feed_dict={tensorimage: image_np_expanded})
       # Visualization of the results of a detection.
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
+          np.squeeze(detected_boxes),
+          np.squeeze(detected_classes).astype(np.int32),
+          np.squeeze(detected_scores),
           category_index,
           use_normalized_coordinates=True,
-          line_thickness=8)
+          line_thickness=4)
 
-      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      cv2.imshow('Tensor Flow', cv2.resize(image_np, (800,600)))
       if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
